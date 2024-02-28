e455ff110ca6958c4d8b54e3d6e1268bdd35f2b8
AmyMcCann <amy.mccann.34@gmail.com>
2024-02-28 11:10:28 +0000

    Added in logic for howToPlay page with forward and back buttons

--- breadVSduck/display/howToPlay/howToPlay.js
+++ breadVSduck/display/howToPlay/howToPlay.js
@@ -1,52 +1,59 @@
-class howToPlay {
-    constructor() {
-        this.currentPageIndex = 0
-        this.displayText = [
+// Sets the current How To Play Instructions page to index 0
+var currentPageIndex = 0
+// An array showing the text for each page of the How To Play Instructions
+var displayText = [
             "Page1 Text",
             "Page2 Text",
             "Page3 Text",
             "Page4 Text"
         ]
-    }
 
-    increasePage () {
-        if (this.currentPageIndex < this.displayText.length) {
-            this.currentPageIndex += 1
+// Next Page Button
+var nextButton = document.getElementById("increasePageButton");
+// Reduce Page Button
+var backButton = document.getElementById("reducePageButton");
+
+// Increases the page number for Instructions
+var increasePage = () => {
+    if (currentPageIndex < displayText.length - 1) {
+            currentPageIndex += 1
+            getDisplayText()
         } 
-        console.log("Page Index once increased: " + this.currentPageIndex)
+        // console.log(currentPageIndex)
+        // console.log(displayText.length)
+    
     }
     
-    reducePage () {
-        if (this.currentPageIndex > 0) {
-            this.currentPageIndex -= 1
+// Decreases the page number for Instructions
+var reducePage = () => {
+        if (currentPageIndex > 0) {
+            currentPageIndex -= 1
+            getDisplayText()
         }
-        console.log("Page Index once decreased: " + this.currentPageIndex)
     }
 
-    getDisplayText () {
-        console.log("display text: " + this.displayText[this.currentPageIndex])
-        return this.displayText[this.currentPageIndex]
+// Gets the text for the relevant Instructions page from the displayText array based on the 'currentPageIndex'
+var getDisplayText = () => {
+    // Hides the back button on the instructions if you are on the first page of instructions
+    if (currentPageIndex == 0) {
+        backButton.style.display = "none"
+    } else {
+        backButton.style.display = "block"
     }
+    // Hides the next button on the instructions if you are on the last page of the instructions
+    if (currentPageIndex == displayText.length - 1) {
+        nextButton.style.display = "none"
+    } else {
+        nextButton.style.display = "block"
+    }
+        var displayTextElement = document.getElementById("displayText");
+        displayTextElement.textContent = displayText[currentPageIndex]
+        return displayText[currentPageIndex]
+    }
+
+var backToHomepage = () => {
+    window.location.href = "../homepage/homepage.html"
 }
 
-var howTo = new howToPlay()
-howTo.getDisplayText();
-howTo.increasePage();
-howTo.increasePage();
-howTo.reducePage();
-howTo.getDisplayText();
-
-// let increaseButton = document.getElementById("increasePageButton")
-// let decreaseButton = document.getElementById("decreasePageButton")
-
-// increaseButton.addEventListener('click', function() {
-//     howTo.increasePage()
-// });
-
-// decreaseButton.addEventListener('click', function() {
-//     howTo.reducePage()
-// });
-
-// var displayTextElement = document.getElementById("displayText")
-
-// displayTextElement.textContent = howTo.getDisplayText()
+// Calls getDisplayText when the html file is first loaded
+getDisplayText()
