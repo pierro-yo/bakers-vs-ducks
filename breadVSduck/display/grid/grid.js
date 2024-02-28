c8e402cb49a686e8a4b5b4e58054b4c3c5d1ac4b
Piers Ford <piers.ford@hotmail.co.ukl>
2024-02-28 11:17:08 +0000

    Added comments, reworked the create grid and handle game grid functions. Used a two dimentional array for easier position locating

--- breadVSduck/display/grid/grid.js
+++ breadVSduck/display/grid/grid.js
@@ -4,8 +4,12 @@ canvas.width = 900
 canvas.height = 600
 
 // global variables
+
+// Each cell row will be 100px wide and 100px tall
 const cellSize = 100   
 const cellGap = 3
+
+// holds data about each individual cell
 const gameGrid = []
 
 // mouse 
@@ -17,6 +21,9 @@ const mouse = {
 }
 
 let canvasPosition = canvas.getBoundingClientRect();
+// returns a dom object relative to the position and size on the canvas
+// this is used for knowing where the mouse is on the canvas
+
 canvas.addEventListener('mousemove', function(e){
     mouse.x = e.x - canvasPosition.left;
     mouse.y = e.y - canvasPosition.top;
@@ -38,6 +45,7 @@ const controlsBar = {
     height: cellSize,
 }
 
+// creates one new cell by taking two arguments of horizontal and vertical values
 class Cell {
     constructor(x, y) {
         this.x = x
@@ -45,7 +53,7 @@ class Cell {
         this.width = cellSize
         this.height = cellSize
     }
-
+// takes the arguments of horizontal and vertical values creates a rectangle of that size at the given coordinates
     draw() {
         if (mouse.x && mouse.y && collision(this, mouse)) {
             ctx.strokeStyle = "black"
@@ -53,21 +61,40 @@ class Cell {
         }
     }  
 }
+// this for loop repesenents rows. It starts at cellsize to give space for the top menu bar
+// it then reaches the end of the row, the outer loop increments 'y' to move down to the next row,
+// and the inner loop starts again for the new row, filling in the positions horizontally.
 function createGrid() {
     for (let y = cellSize; y < canvas.height - cellSize; y += cellSize) {
+        gameRow = []
         for (let x = 0; x < canvas.width; x += cellSize) {
-            gameGrid.push(new Cell(x, y));
+            gameRow.push(new Cell(x, y));
         }
+        gameGrid.push(gameRow)
+        // console.log(gameGrid[1][2])
     }
+// The commented out code is an example of how the game grid can access different cells, the first index refers to the row, the second
+// refers to the column
 }
 
+
+
+// This function iterates over each row (gameRow) in the gameGrid.
 function handleGameGrid() {
-    for (let i = 0; i < gameGrid.length; i++) {
-        gameGrid[i].draw()
+    gameGrid.forEach(function(gameRow) {
+        gameRow.forEach(function(cell) {
+            cell.draw()
+        })
+    })
+    // for (let i = 0; i < gameGrid.length; i++) {
+    //    gameGrid[i].draw()
     }
-}
+// The commented-out code above is an alternative way to achieve the same result using a traditional for loop.
+    // It loops through each element in gameGrid (which represents a row) and calls the draw method for each cell.
+    // However, using forEach is a concise and more modern approach.
 
 function animate() {
+    // this clears the cell, so only the highlighted cell will be shown
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.fillStyle = "gray"
     ctx.fillRect(0, 0, controlsBar.width, controlsBar.height)
@@ -78,9 +105,10 @@ function animate() {
 createGrid()
 animate()
 
+// detects collisions between two rectangles
 function collision(first, second) {
     if (!(first.x > second.x + second.width || first.x +first.width < second.x || first.y > second.y + second.height ||
-       first.y + first.height < second.y)) {
+    first.y + first.height < second.y)) {
         return true
-       }
+    }
 }
