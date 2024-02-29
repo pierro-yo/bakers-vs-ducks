71e7e16d9bf9ebbcfd5735bab8661ce4818a56cc
AmyMcCann <amy.mccann.34@gmail.com>
2024-02-29 10:39:08 +0000

    Altered the homepage so it has a canvas structure like the inplay game view

--- breadVSduck/display/grid/gridHome.js
+++ breadVSduck/display/grid/gridHome.js
@@ -8,10 +8,6 @@ canvas.height = 600
 const cellSize = 100   
 const cellGap = 3
 
-let canvasPosition = canvas.getBoundingClientRect();
-// returns a dom object relative to the position and size on the canvas
-// this is used for knowing where the mouse is on the canvas
-
 // game board
 const controlsBar = {
     width: canvas.width,
@@ -23,13 +19,13 @@ function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.fillStyle = "gray"
     ctx.fillRect(0, 0, controlsBar.width, controlsBar.height)
+
     var textTitle = "Bread vs Ducks"
     textWidth = ctx.measureText(textTitle).width
-
     ctx.font = "30px Comic Sans MS";
     ctx.fillStyle = "black";
     ctx.textAlign = "center";
-    ctx.fillText(textTitle, (canvas.width/2), controlsBar.height/2);
+    ctx.fillText(textTitle, (canvas.width/2), controlsBar.height/2 + 10);
     requestAnimationFrame(animate)
 }
 
